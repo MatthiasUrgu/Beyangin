231e28df946bf5d1b900182042f88780f7d0f5a7
Matthias.Urgu <matthias.urgu@gmail.com>
2023-04-03 16:26:35 +0200

    changement classe sur buttonGwenerator

--- src/components/ButtonGenearator/ButtonGenearator.jsx
+++ src/components/ButtonGenearator/ButtonGenearator.jsx
@@ -2,7 +2,7 @@ import s from './style.module.scss'
 
 function ButtonGenearator(items){
     const buttons = Object.keys(items).map((key)=>
-   <button className={s.button} key={key} type="button">{items[key]}</button> )
+   <button className={s.buttonClick} key={key} type="button">{items[key]}</button> )
 
     return <div>{buttons}</div>;
         
