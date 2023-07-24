1a12d5a7a8d2ce0e2af228fd8c0ba3fbd9ab531a
Matthias.Urgu <matthias.urgu@gmail.com>
2023-07-24 14:35:01 +0200

    Fix bug

--- src/components/header-menu/header-menu.jsx
+++ src/components/header-menu/header-menu.jsx
@@ -1,5 +1,16 @@
 import s from "./style.module.scss";
+import { useState } from "react";
 function HeaderMenu({ list1, list2, list3, list4 }) {
+
+  const [ Hovered, setIsHovered] = useState('')
+ 
+  const buttonClass = Hovered ? `` : `désactiver`;
+  const classover = Hovered ? s.hamburgerOn : s.hamburgerOff
+  function Toggle(){
+    setIsHovered(!Hovered)
+    console.log(Hovered);
+  }
+
   return (
     <div className={s.menu}>
       <nav className={s.navMenu}>
@@ -18,17 +29,26 @@ function HeaderMenu({ list1, list2, list3, list4 }) {
       </nav>
       <div className={s.hamburger}>
 
-        <button className={s.hamburgerButton}>
+        <button className={`${s.hamburgerButton} ${buttonClass}`} onClick={Toggle}>
+          {/* {Hovered ? `activer` : 'désactiver'} */}
             <span className={s.hamburgerHide}></span>
             <span className={s.hamburgerHide}></span>
             <span className={s.hamburgerHide}></span>
             
         </button>
 
-        <section className={s.hamburgerOn}></section>
+        <section className={`${classover} ${s.hamburgerOpen}`}>
+        {/* {Hovered ? `hamburgerOn` : 'hamburgerOff'} */}
+        <ol>
+          <li><a href="home">{list1} </a></li>
+          <li><a href="about">{list2}</a></li>
+          <li><a href="library">{list3}</a></li>
+          <li><a href="contact">{list4}</a></li>
+        </ol>
+        </section>
       </div>
     </div>
-  );
+);
 }
 
 export default HeaderMenu;
